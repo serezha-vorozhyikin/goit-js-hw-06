9fb61b79fbf5903bd1c68be026ccbb47c7a3fd6b
serezha-vorozhyikin <serezhavorozhyikin96@gmail.com>
2022-12-03 01:07:59 +0200

    hw-6

--- js/task-01.js
+++ js/task-01.js
@@ -1,2 +1,7 @@
-const list = document.querySelector(`#categories`);
-console.log(`Number of categories ${list.children.length}`);
+const list = document.querySelectorAll(".item");
+console.log(`Number of categories: ${list.length}`);
+
+list.forEach((element) => {
+  console.log("Category: ", element.querySelector("h2").textContent);
+  console.log("Elements: ", element.querySelectorAll("li").length);
+});
